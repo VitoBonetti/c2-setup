b8bb7d290e910eb029c7469c70d4a2ea2108b1d7
VitoBonetti <99612766+VitoBonetti@users.noreply.github.com>
2024-07-03 19:32:31 +0200

    Update pack_checker_test.sh

--- pack_checker_test.sh
+++ pack_checker_test.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-packages_list_dpkg = ["python3-venv", ]
+packages_list_dpkg = ("snapd" "python3-venv" "net-tools" "git" "plocate" "apache2" "hashcat" "gobuster" "dirb" "hping3" "john" "cewl" "smbmap" "socat" "screen" "whatweb" "sendemail" "unzip")
+packages_list_snap = ("go" "nmap" "rustscan" "sqlmap" "powershell" "enum4linux")
+
+for package in "${packages_list_dpkg[@]}"; do
+    if dpkg -s "$package" > /dev/null 2>&1; then
+        echo "[+] $package correctly installed"
+    else
+        echo "[-] $package was not correctly installed"
+    fi
+done
+
+for package in "${packages_list_snap[@]}"; do
+    if snap list | grep "$package" > /dev/null 2>&1; then
+        echo "[+] $package correctly installed"
+    else
+        echo "[-] $package was not correctly installed"
+    fi
+done
