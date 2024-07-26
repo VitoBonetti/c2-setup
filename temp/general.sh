5973aa7b9ccbb235316f19d1b807d88da8264430
VitoBonetti <99612766+VitoBonetti@users.noreply.github.com>
2024-07-26 08:01:33 +0200

    Update general.sh

--- temp/general.sh
+++ temp/general.sh
@@ -15,7 +15,7 @@ install_package() {
     if dpkg -s "${package}" > /dev/null 2>&1; then
         echo -e "${G}[+] ${package} is already installed!${N}"
     else
-        if sudo apt install -y "${package}"; then
+        if sudo apt install -y "${package} > 2>/dev/null"; then
             if dpkg -s "${package}" > /dev/null 2>&1; then
                 echo -e "${G}[+] ${package} installed successfully!${N}"
             else
@@ -72,37 +72,36 @@ echo -e "${N}"
 echo -e "${O}[*] Determining linux distribution...${N}"
 
 DESCRIPTION=$(lsb_release -d | awk -F'\t' '{print $2}')
-if [[ "$DESCRIPTION" == *"Mint"* ]]; then
-    OS="Mint"
-elif [[ "$DESCRIPTION" == *"Debian"* ]]; then
-    OS="Debian"
-elif [[ "$DESCRIPTION" == *"Ubuntu"* ]]; then
-    OS="Ubuntu"
-else
-    OS="Other"
-fi
 
 echo -e "${G}[+] ${DESCRIPTION} ${N}"
+
 echo -e "${O}[*] Updating the system...${N}"
 sudo apt update -y
+echo -e "${G}[+] Done! ${N}"
 echo -e "${O}[*] Upgrading the system...${N}"
+echo -e "${G}[+] Done! ${N}"
 sudo apt upgrade -y
-
-if [[ $OS == "Mint" ]]; then
-    sudo add-apt-repository universe
-    sudo apt update -y
-fi
 
 echo -e "${O}[*] Installing development tools...${N}"
 
 install_package "build-essential"
-install_package "libc6-dev"
-install_package "apt-utils"
-install_package "libsasl2-dev" 
-install_package "python3-dev" 
-install_package "libldap2-dev" 
+install_package "libsasl2-dev"
+install_package "python3-dev"
+install_package "libldap2-dev"
 install_package "libssl-dev"
+install_package "net-tools"
+install_package "python3-venv"
+sudo ln -s /usr/bin/python3 /usr/bin/python
+
+if [[ $DESCRIPTION == "Ubuntu 20.04.6 LTS"]]; then
+    install_package "mlocate"
+else
+    install_package "plocate"
+fi
+
+echo -e "${O}[*] Reloding daemon...${N}"
 sudo systemctl daemon-reload
+echo -e "${G}[+] Done! ${N}"
 
 echo -e "${O}[*] Make the /tmp folder persistence...${N}"
 sudo touch /etc/tmpfiles.d/tmp.conf
@@ -110,25 +109,21 @@ echo "# Disable automatic cleanup of /tmp" | sudo tee /etc/tmpfiles.d/tmp.conf
 echo "d /tmp 1777 root root -" | sudo tee -a /etc/tmpfiles.d/tmp.conf
 echo -e "${G}[+] Done!${N}"
 
-echo -e "${O}[*] Creating Git folder in '/home/${currenr_user}'... ${N}"
+echo -e "${O}[*] Creating Git folder in '/home/${current_user}'... ${N}"
 mkdir Git
 echo -e "${G}[+] Done!${N}"
 
-if [[ $OS == "Mint" ]]; then
-    sudo ln -s /usr/bin/python3 /usr/bin/python 
-fi
-
-install_package "python3-venv"
-
 echo -e "${O}[*] Creating Python virtual environment.${N}"
 if python -m venv env; then
-	sleep 5
-	echo -e "${G}[+]Python virtual environment created successfully!${N}"
-	if source env/bin/activate; then
+    sleep 5
+    echo -e "${G}[+] Python virtual environment created successfully at '/home/${current_user}/env'.${N}"
+    if source env/bin/activate; then
 		if [ -z "$VIRTUAL_ENV" ]; then
     		echo -e "${R}[-] No virtual environment is active.${N}"
 		else
 			echo -e "${G}[+] Virtual environment is active: $VIRTUAL_ENV${N}"
+            echo -e "${O}[*] Deactivating.${N}"
+            deactivate
 		fi
 	else
 		echo -e "${R}"
@@ -147,36 +142,13 @@ else
 	exit 1
 fi
 
-echo -e "${O}[*] Installing snap.${N}"
-if [[ $OS == "Mint" ]]; then
-    wget http://security.ubuntu.com/ubuntu/pool/main/s/snapd/snapd_2.58+22.04.1_amd64.deb  
-    sudo dpkg -i snapd_2.58+22.04.1_amd64.deb 
-    if snap version; then
-	    echo -e "${G}[+] snap  installed successfully!${N}"
-        rm -f snapd_2.58+22.04.1_amd64.deb 
-    else
-        echo -e "${R}"
-        echo "[-] Failed to install snap!"
-        echo "[-] The script can't continue."
-        echo "[-] Terminating.."
-        echo -e "${N}"
-        exit 1
-    fi
-else
-    sudo apt install snapd -y
-    if snap version  > /dev/null 2>&1; then
-        echo -e "${G}[+] snap  installed successfully!${N}"
-    else
-        echo -e "${R}"
-        echo "[-] Failed to install snap!"
-        echo "[-] The script can't continue."
-        echo "[-] Terminating.."
-        echo -e "${N}"
-        exit 1
-    fi
+if [[ $DESCRIPTION == "Debian*"]]; then
+    install_package "snapd"
+    echo 'export PATH=$PATH:/snap/bin:/usr/sbin' >> ~/.profile
+    source ~/.profile
+    install_package "git"
 fi
 
-install_package "git"
 install_package "apache2"
 
 if sudo systemctl is-active --quiet apache2; then
@@ -189,30 +161,78 @@ else
         echo -e "${R}[-] Failed to start apache2 web server!${N}"
 	    echo "[>] Continuing..."
     fi
+fi    
+
+echo -e "${O}[*] Installing docker.io...${N}"
+ if dpkg -s docker.io > /dev/null 2>&1; then
+    echo -e "${G}[+] docker.io is already installed!${N}"
+else
+    if sudo apt install -y "docker.io"; then
+        if dpkg -s docker.io > /dev/null 2>&1; then
+             echo -e "${G}[+] docker.io installed successfully!${N}"
+        else
+            echo -e "${R}"
+		    echo "[-] Failed to verify docker.io installation!"
+		    echo "[-] The script can't continue."
+		    echo "[-] Terminating.."
+		    echo -e "${N}"
+		    exit 1
+        fi 
+    else
+        echo -e "${R}"
+		echo "[-] Failed to install docker.io!"
+		echo "[-] The script can't continue."
+		echo "[-] Terminating.."
+		echo -e "${N}"
+		exit 1
+    fi
+fi 
+if sudo docker run hello-world; then
+	echo -e "${G}[+] docker server is up and running!${N}"
+else
+    sudo systemctl start docker
+    if sudo docker run hello-world; then
+	    echo -e "${G}[+] docker server is up and running!${N}"
+    else
+        echo -e "${R}"
+		echo "[-] Failed to activate docker.io!"
+		echo "[-] The script can't continue."
+		echo "[-] Terminating.."
+		echo -e "${N}"
+		exit 1
+    fi
 fi
 
-install_package "docker.io"
-sudo docker run hello-world
 
+echo -e "${O}[*] Installing tools with APT...${N}"
 install_package "hashcat"
 install_package "hydra-gtk"
 install_package "gobuster"
 install_package "dirb"
 install_package "hping3"
-
 install_package "john"
 install_package "cewl"
 install_package "smbmap"
-install_package "socat"
-install_package "screen"
 install_package "whatweb"
 install_package "sendemail"
-install_package "unzip"
-
-install_package "ruby-rubygems"
 install_package "ruby-dev"
 
+if [[ DESCRIPTION == "Ubuntu*"]]; then
+    install_package "socat"
+fi
 
+echo -e "${G}[+] Done! ${N}"
+
+echo -e "${O}[*] Installing tools with GEMS...${N}"
+if sudo gem install wpscan; then
+    echo -e "${G}[+] wpscan successfully installed!${N}"
+else
+    echo -e "${R}[-] Failed to install wpscan!${N}"
+    echo "[>] Continuing..."
+fi
+echo -e "${G}[+] Done! ${N}"
+
+echo -e "${O}[*] Installing tools with SNAP...${N}"
 install_snap "go"
 install_snap "nmap"
 install_snap "rustscan"
@@ -220,75 +240,82 @@ install_snap "sqlmap"
 install_snap "enum4linux"
 install_snap "powershell"
 
-echo -e "${O}[*] Installing wpscan...${N}"
-sudo gem install wpscan
-if wpscan --version; then
-	echo -e "${G}[+] wpscan installed successfully!${N}"
-else
-	echo -e "${R}[-] Failed to install wpscan!${N}"
-	echo "[>] Continuing..."
-fi
+echo -e "${G}[+] Done! ${N}"
 
-echo -e "${O}[*] Installing wine...${N}"
-dpkg --print-architecture
-sudo dpkg --add-architecture i386
-dpkg --print-foreign-architectures
-sudo mkdir -pm755 /etc/apt/keyrings
-sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
-sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bookworm/winehq-bookworm.sources
-sudo apt update -y
-sudo apt install --install-recommends winehq-stable -y
-if wine --version; then
-	echo -e "${G}[+] wine installed successfully!${N}"
-else
-	echo -e "${R}[-] Failed to install wine!${N}"
-	echo "[>] Continuing..."
-fi
-
-if [ -z "$VIRTUAL_ENV" ]; then
-    cd
-    source env/bin/activate
-    echo -e "${O}[*] Installing certipy-ad...${N}"
-    if pip install wheel lxml==4.9.3 certipy-ad; then
-        echo -e "${G}[+] certipy-ad installed successfully!${N}"
+if [[ DESCRIPTION == "Ubuntu*"]]; then
+    install_package "wine64"
+elif [[ DESCRIPTION == "Debian GNU Linux 11 (bullseye)"]]; then
+    echo -e "${O}[*] Installing wine...${N}"
+    dpkg --print-architecture
+    sudo dpkg --add-architecture i386
+    dpkg --print-foreign-architectures
+    sudo mkdir -pm755 /etc/apt/keyrings
+    sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+    sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bullseye/winehq-bullseye.sources
+    sudo apt update -y 2>/dev/null
+    if sudo apt install --install-recommends winehq-stable -y 2>/dev/null; then
+        echo -e "${G}[+] wine successfully installed!${N}"
     else
-        echo -e "${R}[-] Failed to install certipy-ad!${N}"
+        echo -e "${R}[-] Failed to install wine!${N}"
         echo "[>] Continuing..."
     fi
-    echo -e "${O}[*] Installing kerbrute...${N}"
-    pip install kerbrute
-    pip install --upgrade setuptools
-    if kerbrute --help; then
-        echo -e "${G}[+] kerbrute installed successfully!${N}"
+elif [[ DESCRIPTION == "Debian GNU Linux 12 (bookworm)"]]; then
+    echo -e "${O}[*] Installing wine...${N}"
+    dpkg --print-architecture
+    sudo dpkg --add-architecture i386
+    dpkg --print-foreign-architectures
+    sudo mkdir -pm755 /etc/apt/keyrings
+    sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+    sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bookworm/winehq-bookworm.sources
+
+    sudo apt update -y 2>/dev/null
+    if sudo apt install --install-recommends winehq-stable -y 2>/dev/null; then
+        echo -e "${G}[+] wine successfully installed!${N}"
     else
-        echo -e "${R}[-] Failed to install kerbrute!${N}"
-        echo "[>] Continuing..."
-    fi	
-    echo -e "${B}[*] Installing bloodhound...${N}"
-    pip install bloodhound 
-    if bloodhound-python --help; then
-        echo -e "${G}[+]bloodhound installed successfully!${N}"   
-	else
-		echo -e "${R}[-] Failed to install bloodhound!${N}"
-		echo "[>] Continuing..."
-	fi
-else
-    echo -e "${O}[*] Installing certipy-ad...${N}"
-    if pip install wheel lxml==4.9.3 certipy-ad; then
-        echo -e "${G}[+] certipy-ad installed successfully!${N}"
-    else
-        echo -e "${R}[-] Failed to install certipy-ad!${N}"
+        echo -e "${R}[-] Failed to install wine!${N}"
         echo "[>] Continuing..."
     fi
-    echo -e "${O}[*] Installing kerbrute...${N}"
-    pip install kerbrute
-    pip install --upgrade setuptools
-    if kerbrute --help; then
-        echo -e "${G}[+] kerbrute installed successfully!${N}"
-    else
-        echo -e "${R}[-] Failed to install kerbrute!${N}"
-        echo "[>] Continuing..."
-    fi	
+fi
+
+source env/bin/activate
+
+echo -e "${O}[*] Installing certipy-ad...${N}"
+pip install wheel
+pip install lxml==4.9.3
+pip install --upgrade setuptools
+pip install certipy-ad
+
+if certipy -h; then
+    echo -e "${G}[+] certipy-ad installed successfully!${N}"
+else
+    echo -e "${R}[-] Failed to install certipy-ad!${N}"
+    echo "[>] Continuing..."
+fi
+
+
+echo -e "${O}[*] Installing kerbrute...${N}"
+pip install kerbrute
+pip install --upgrade setuptools
+if pip install wheel lxml==4.9.3 certipy-ad; then
+    echo -e "${G}[+] certipy-ad installed successfully!${N}"
+else
+    echo -e "${R}[-] Failed to install certipy-ad!${N}"
+    echo "[>] Continuing..."
+fi
+
+echo -e "${B}[*] Installing bloodhound...${N}"
+pip install bloodhound 
+if bloodhound-python --help; then
+    echo -e "${G}[+]bloodhound installed successfully!${N}"   
+else
+    echo -e "${R}[-] Failed to install bloodhound!${N}"
+    echo "[>] Continuing..."
+fi
+
+deactivate
+
+if [[ DESCRIPTION == "Debian*"]]; then
+    source ~/.profile
 fi
 
 echo -e "${B}[*] Installing garble...${N}"
@@ -299,9 +326,7 @@ else
 	echo "[>] Continuing..."
 fi
 
-echo -e "${O}[*] Docker Container Apps ...${N}"
-
-
+echo -e "${O}[*] Pulling containers from DOCKER...${N}"
 echo -e "${B}[*] Installing manspider...${N}"
 sudo docker pull blacklanternsecurity/manspider
 if sudo docker run blacklanternsecurity/manspider --help; then
@@ -320,8 +345,8 @@ else
 	echo "[>] Continuing..."
 fi
 
-cd Git
 echo -e "${O}[*] Cloning NetExec...${N}"
+cd Git
 git clone https://github.com/Pennyw0rth/NetExec
 cd NetExec
 echo -e "${B}[*] Installing NetExec...${N}"
@@ -334,8 +359,8 @@ else
 fi
 cd
 
-cd Git
 echo -e "${O}[*] Cloning nikto...${N}"
+cd Git
 git clone https://github.com/sullo/nikto.git
 cd nikto
 echo -e "${B}[*] Installing nikto...${N}"
@@ -348,46 +373,34 @@ else
 fi
 cd
 
+echo -e "${G}[+] Done! ${N}"
 
-echo -e "${O}[*] Cloning repositories...${N}"
-if [ -z "$VIRTUAL_ENV" ]; then
-    source /home/${currenr_user}/env/bin/activate
-fi
+echo -e "${O}[*] Cloning repositories from GIT...${N}"
 cd Git
 echo -e "${O}[*] Cloning Ligolo proxy...${N}"
 git clone https://github.com/0x00-0x00/ligolo-ng
 cd ligolo-ng
-go build -o lg-proxy cmd/proxy/main.go
-if lg-proxy -h; then
-    cd ..
-	echo -e "${G}[+] ligolo proxy installed successfully!${N}"
+if go build -o lg-proxy cmd/proxy/main.go; then
+    sudo ln -s /home/$current_user/Git/ligolo-ng/lg-proxy /usr/bin/lg-proxy
+    if lg-proxy -h; then
+        echo -e "${G}[+] ligolo proxy installed successfully!${N}"
+    else
+        echo -e "${R}"
+	    echo "[-] Failed to install ligolo proxy!"
+	    echo "[-] The script can't continue."
+	    echo "[-] Terminating.."
+	    echo -e "${N}"
+	    exit 1
+    fi
 else
-	echo -e "${R}"
-	echo "[-] Failed to install ligolo proxy!"
+    echo -e "${R}"
+	echo "[-] Failed to build ligolo proxy!"
 	echo "[-] The script can't continue."
 	echo "[-] Terminating.."
 	echo -e "${N}"
 	exit 1
 fi
-
-echo -e "${O}[*] Create ligolo TUN adapter for the tunnel...${N}"
-sudo ip tuntap add user "$current_user" mode tun ligolo
-sudo ip link set ligolo up
-if ifconfig ligolo; then
-	echo -e "${G}[+] ligolo TUN adapter created successfully!${N}"
-	echo -e "${B}"
-	echo "You need to add a new route to the ligolo TUN adapter."
-	echo "You  need to bind the Domain controller IP address to the name in the /etc/hosts file."
-	echo "This it will change depending from the target."
-	echo -e "${N}"
-else
-	echo -e "${R}"
-	echo "[-] Failed to install ligolo TUN adapter!"
-	echo "[-] The script can't continue."
-	echo "[-] Terminating.."
-	echo -e "${N}"
-	exit 1
-fi
+cd ..
 
 echo -e "${O}[*] Cloning Ligolo agent...${N}"
 if git clone https://github.com/nicocha30/ligolo-ng ligolo-agent; then
@@ -397,10 +410,14 @@ else
 	echo "[>] Continuing..."
 fi
 
+if [ -z "$VIRTUAL_ENV" ]; then
+    source /home/${currenr_user}/env/bin/activate
+fi
+
 echo -e "${O}[*] Cloning dnschef...${N}"
 git clone https://github.com/iphelix/dnschef.git
-echo -e "${O}[*] Installing dnschef...${N}"
 cd dnschef
+echo -e "${O}[*] Installing dnschef dependencies...${N}"
 pip install -r requirements.txt 
 if python dnschef.py -h; then
     echo -e "${G}[+] dnschef installed successfully!${N}"
@@ -408,12 +425,12 @@ else
     echo -e "${R}[-] Failed to install dnschef!${N}"
     echo "[>] Continuing..."
 fi
-cd..
+cd ..
 
 echo -e "${O}[*] Cloning ldap-scanner...${N}"
 git clone https://github.com/GoSecure/ldap-scanner.git 
-echo -e "${O}[*] Installing ldap-scanner...${N}"
 cd ldap-scanner
+echo -e "${O}[*] Installing ldap-scanner dependencies...${N}"
 pip install impacket 
 pip install --upgrade setuptools
 if python ldap-scanner.py -h; then
@@ -422,12 +439,12 @@ else
     echo -e "${R}[-] Failed to install ldap-scanner!${N}"
     echo "[>] Continuing..."
 fi
-cd..
+cd ..
 
 echo -e "${O}[*] Cloning adidnsdump...${N}"
 git clone https://github.com/dirkjanm/adidnsdump 
-echo -e "${O}[*] Installing adidnsdump ...${N}"
 cd adidnsdump
+echo -e "${O}[*] Installing adidnsdump dependencies...${N}"
 pip install . 
 if adidnsdump -h; then
     echo -e "${G}[+] adidnsdump installed successfully!${N}"
@@ -435,23 +452,21 @@ else
     echo -e "${R}[-] Failed to install adidnsdump!${N}"
     echo "[>] Continuing..."
 fi
-cd..
-
+cd ..
 
 echo -e "${O}[*] Cloning ADenum...${N}"
 git clone https://github.com/pelletierr/ADenum/
-echo -e "${O}[*] Installing ADenum ...${N}"
 cd ADenum
+echo -e "${O}[*] Installing ADenum dependencies...${N}"
 pip install wheel
-pip install -r requirements.tx
-if ADenum.py -h; then
+pip install -r requirements.txt
+if python ADenum.py -h; then
     echo -e "${G}[+] ADenum installed successfully!${N}"
 else
     echo -e "${R}[-] Failed to install ADenum!${N}"
     echo "[>] Continuing..."
 fi
-cd..
-
+cd ..
 
 echo -e "${O}[*] Cloning gmapsapiscanner...${N}"
 git clone https://github.com/ozguralp/gmapsapiscanner.git
@@ -463,6 +478,7 @@ else
 	echo "[>] Continuing..."
 fi	
 cd
+deactivate
 
 echo -e "${B}[*] Installing SecLists...${N}"
 sudo mkdir /usr/share/wordlists
@@ -473,7 +489,6 @@ sudo chmod 755 /usr/share/wordlists
 sudo chmod -R a+r /usr/share/wordlists
 sudo find /usr/share/wordlists -type d -exec chmod 755 {} \;
 echo -e "${G}[+] SecLists installed successfully!${N}"
-
 
 echo -e "${O}[*] Updating database...${N}"
 sudo updatedb
