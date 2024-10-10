ce4d2e892461e1835dbcd0e91a10e195dc22b6b2
VitoBonetti <99612766+VitoBonetti@users.noreply.github.com>
2024-10-10 17:14:30 +0200

    Update multiplatform.sh

--- multiplatform.sh
+++ multiplatform.sh
@@ -15,7 +15,8 @@ install_apt() {
     for package in "${packages[@]}"; do
 	    echo -e "${B}[*] Installing $package...${N}"
 	    if dpkg -s "$package" > /dev/null 2>&1; then
-	        echo -e "${G}[+] $package is already installed!${N}"
+	        echo -e "${C}[>] $package is already installed!${N}"
+	        echo -e "${C}[>] Continuing...${N}"
 	    else
 	        if sudo apt install -y "$package" > /dev/null 2>&1; then
 	            if dpkg -s "$package" > /dev/null 2>&1; then
@@ -37,7 +38,8 @@ install_snap() {
     for package in "${packages[@]}"; do
 	    echo -e "${B}[*] Installing $package...${N}"
 	    if snap list | grep "$package" > /dev/null 2>&1; then
-	        echo -e "${G}[+] $package is already installed!${N}"
+	        echo -e "${C}[>] $package is already installed!${N}"
+	        echo -e "${C}[>] Continuing...${N}"
 	    else
 	        if sudo snap install "${package}"; then
 	            if snap list | grep "$package" > /dev/null 2>&1; then
@@ -156,8 +158,9 @@ if [[ $os_version == *"Ubuntu"* ]]; then
             install_apt ${ubuntu[$key]}
 
 			if [[ $os_version == *"24.04"* ]]; then
+				echo -e "${B}[*] Installing plocate... ${N}"
 				if dpkg -s "plocate" > /dev/null 2>&1; then
-			        echo -e "${G}[+] plocate is already installed!${N}"
+			        echo -e "${C}[>] plocate is already installed!${N}"
 			    else
 			        if sudo apt install -y plocate > /dev/null 2>&1; then
 			            if dpkg -s plocate > /dev/null 2>&1; then
@@ -172,8 +175,9 @@ if [[ $os_version == *"Ubuntu"* ]]; then
 			        fi
 			    fi
 			else
+				echo -e "${B}[*] Installing mlocate... ${N}"
 				if dpkg -s "mlocate" > /dev/null 2>&1; then
-			        echo -e "${G}[+] mlocate is already installed!${N}"
+			        echo -e "${C}[>] mlocate is already installed!${N}"
 			    else
 			        if sudo apt install -y mlocate > /dev/null 2>&1; then
 			            if dpkg -s mlocate > /dev/null 2>&1; then
@@ -199,6 +203,8 @@ if [[ $os_version == *"Ubuntu"* ]]; then
             python3 -m venv ~/env
             sleep 3
             source ~/env/bin/activate
+			echo -e "${O}[*] Upgrading PIP${N}"
+			pip install --upgrade pip
 
             echo -e "${O}[*] Installing packages with PIP${N}"
             install_pip ${ubuntu[$key]}
@@ -234,7 +240,8 @@ elif [[ $os_version == *"Debian"* ]]; then
             python3 -m venv ~/env
             sleep 3
             source ~/env/bin/activate
-
+			echo -e "${O}[*] Upgrading PIP${N}"
+			pip install --upgrade pip
             echo -e "${O}[*] Installing packages with PIP${N}"
             install_pip ${debian[$key]}
             sleep 3
@@ -259,15 +266,15 @@ else
 fi
 
 echo -e "${B}[*] Installing ${N} ${O}GO${N}"
-wget https://go.dev/dl/go1.23.2.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.23.2.linux-amd64.tar.gz
+wget https://go.dev/dl/go1.23.2.linux-amd64.tar.gz > /dev/null 2>&1
+sudo tar -C /usr/local -xzf go1.23.2.linux-amd64.tar.gz 
 export PATH=$PATH:/usr/local/go/bin
 source ~/.bashrc 
 rm go1.23.2.linux-amd64.tar.gz
 echo -e "${G}Done!${N}"
 
 echo -e "${B}[*] Installing garble...${N}"
-if go install mvdan.cc/garble@master; then
+if go install mvdan.cc/garble@master > /dev/null 2>&1; then
 	echo -e "${G}[+] garble installed successfully!${N}"
 else
 	echo -e "${R}[-] Failed to install garble!${N}"
@@ -276,20 +283,28 @@ fi
 cd ~/Git
 cd NetExec
 echo -e "${B}[*] Installing NetExec...${N}"
-sudo docker build -t netexec:latest .
-if sudo docker run netexec --help > /dev/null 2>&1; then
-	echo -e "${G}[+] netexec installed successfully!${N}"
+if sudo docker build -t netexec:latest . > /dev/null 2>&1; then
+	if sudo docker run netexec --help > /dev/null 2>&1; then
+		echo -e "${G}[+] netexec installed successfully!${N}"
+	else
+		echo -e "${R}[-] Failed to install netexec!${N}"
+		echo -e "${C}[>] Continuing...${N}"
+	fi
 else
 	echo -e "${R}[-] Failed to install netexec!${N}"
-	echo -e "${C}[>] Continuing...${N}"
+	echo -e "${C}[>] Continuing...${N}"	
 fi
 cd ..
 
 cd nikto
 echo -e "${B}[*] Installing nikto...${N}"
-sudo docker build -t sullo/nikto .
-if sudo docker run --rm sullo/nikto -Version > /dev/null 2>&1; then
-	echo -e "${G}[+] nikto installed successfully!${N}"
+if sudo docker build -t sullo/nikto . > /dev/null 2>&1; then
+	if sudo docker run --rm sullo/nikto -Version > /dev/null 2>&1; then
+		echo -e "${G}[+] nikto installed successfully!${N}"
+	else
+		echo -e "${R}[-] Failed to install nikto!${N}"
+		echo -e "${C}[>] Continuing...${N}"
+	fi
 else
 	echo -e "${R}[-] Failed to install nikto!${N}"
 	echo -e "${C}[>] Continuing...${N}"
@@ -297,7 +312,8 @@ fi
 cd ..
 
 cd ligolo-ng
-if go build -o lg-proxy cmd/proxy/main.go; then
+echo -e "${B}[*] Installing ligolo proxy...${N}"
+if go build -o lg-proxy cmd/proxy/main.go > /dev/null 2>&1; then
 	sudo ln -s /home/$USER/Git/ligolo-ng/lg-proxy /usr/bin/lg-proxy
 	echo -e "${G}[+] ligolo proxy installed successfully!${N}"
 else
@@ -309,42 +325,51 @@ cd ..
 source ~/env/bin/activate
 echo -e "${B}[*] Installing python package for${N} ${O}dnschef, adidnsdump, ADenum, ROADtools${N} ${B}tools${N}"
 cd dnschef
-pip install -r requirements.txt
+pip install -r requirements.txt > /dev/null 2>&1
 cd ..
 cd adidnsdump
-pip install .
+pip install . > /dev/null 2>&1
 cd ..
 cd ADenum
-pip install -r requirements.txt
+pip install -r requirements.txt > /dev/null 2>&1
 cd ..
 cd ROADtools/
-pip install .
+pip install . > /dev/null 2>&1
 cd 
-echo -e "${G}Done!${N}"
+echo -e "${G}[+] Done!${N}"
 deactivate
 
 cd ~/Tools
 mkdir routes
 cd routes
 echo -e "${B}[*] Downloading 'routes' tool of @Andre Marques...${N}"
-wget https://raw.githubusercontent.com/VitoBonetti/c2-setup/refs/heads/main/routes/configure_routes.sh
-wget https://raw.githubusercontent.com/VitoBonetti/c2-setup/refs/heads/main/routes/ipparser.py
+wget https://raw.githubusercontent.com/VitoBonetti/c2-setup/refs/heads/main/routes/configure_routes.sh > /dev/null 2>&1
+wget https://raw.githubusercontent.com/VitoBonetti/c2-setup/refs/heads/main/routes/ipparser.py > /dev/null 2>&1
 cd
-echo -e "${G}Done!${N}"
+echo -e "${G}[+] Done!${N}"
 
 echo -e "${B}[*] Installing manspider...${N}"
-sudo docker pull blacklanternsecurity/manspider
-if sudo docker run blacklanternsecurity/manspider --help > /dev/null 2>&1; then
-	echo -e "${G}[+] maspider installed successfully!${N}"
+if sudo docker pull blacklanternsecurity/manspider > /dev/null 2>&1; then
+	if sudo docker run blacklanternsecurity/manspider --help > /dev/null 2>&1; then
+		echo -e "${G}[+] maspider installed successfully!${N}"
+	else
+		echo -e "${R}[-] Failed to install manspider!${N}"
+		echo "[>] Continuing..."
+	fi
 else
 	echo -e "${R}[-] Failed to install manspider!${N}"
 	echo "[>] Continuing..."
 fi
 
+
 echo -e "${B}[*] Installing gowitness...${N}"
-sudo docker pull leonjza/gowitness
-if sudo docker run --rm leonjza/gowitness gowitness > /dev/null 2>&1; then
-	echo -e "${G}[+] gowitness installed successfully!${N}"
+if sudo docker pull leonjza/gowitness > /dev/null 2>&1; then
+	if sudo docker run --rm leonjza/gowitness gowitness > /dev/null 2>&1; then
+		echo -e "${G}[+] gowitness installed successfully!${N}"
+	else
+		echo -e "${R}[-] Failed to install gowitness!${N}"
+		echo "[>] Continuing..."
+	fi
 else
 	echo -e "${R}[-] Failed to install gowitness!${N}"
 	echo "[>] Continuing..."
@@ -370,12 +395,16 @@ sudo mkdir /usr/share/wordlists
 vito@debian:~$ 
 
 cd /usr/share/wordlists
-sudo git clone https://github.com/danielmiessler/SecLists.git
-cd
-sudo chmod 755 /usr/share/wordlists
-sudo chmod -R a+r /usr/share/wordlists
-sudo find /usr/share/wordlists -type d -exec chmod 755 {} \;
-echo -e "${G}Done!${N}"
+if sudo git clone https://github.com/danielmiessler/SecLists.git > /dev/null 2>&1; then
+	cd
+	sudo chmod 755 /usr/share/wordlists
+	sudo chmod -R a+r /usr/share/wordlists
+	sudo find /usr/share/wordlists -type d -exec chmod 755 {} \;
+	echo -e "${G}Done!${N}"
+else
+	echo -e "${R}[-] Failed to clone  SecLists repository !${N}"
+	echo -e "${C}[>] Continuing...${N}"
+fi
 
 echo -e "${B}[*] Updating Database...${N}"
 sudo updatedb
@@ -384,4 +413,5 @@ end_time=$(date +%s)
 execution_time=$((end_time - start_time))
 echo -e "${G}[+] Done${N}"
 echo -e "${0}[+] Execution time: $execution_time seconds${N}"
+echo -e "${P}[#] Bye Bye $USER have a great hacking time!${N}"
 exit 0
