d11256767234c760cba8004346de73f833b7f6e4
VitoBonetti <99612766+VitoBonetti@users.noreply.github.com>
2024-10-10 17:23:21 +0200

    Update multiplatform.sh

--- multiplatform.sh
+++ multiplatform.sh
@@ -265,7 +265,7 @@ else
 	 exit 1
 fi
 
-echo -e "${B}[*] Installing ${N} ${O}GO${N}"
+echo -e "${B}[*] Installing GO${N}"
 wget https://go.dev/dl/go1.23.2.linux-amd64.tar.gz > /dev/null 2>&1
 sudo tar -C /usr/local -xzf go1.23.2.linux-amd64.tar.gz 
 export PATH=$PATH:/usr/local/go/bin
